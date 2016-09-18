b4ff8837209464b5f31ba27eeff14b57d8a8c096
zelinskiy <zelinskiy1917@gmail.com>
2016-09-18 16:16:14 +0300

    Initial commit, with basic preview

---
+++ Astral/Model.fs
@@ -0,0 +1,57 @@
+﻿module Models
+
+open Suave
+open Suave.Json
+open System.Runtime.Serialization
+
+[<DataContract>]
+type Customer = 
+    { 
+        [<field: DataMember(Name="Id")>]
+        Id:int;
+
+        [<field: DataMember(Name="Name")>]
+        Name:string; 
+    }
+
+[<DataContract>]
+type Commodity = 
+    {
+        [<field: DataMember(Name="Id")>]
+        Id:int;
+
+        [<field: DataMember(Name="Name")>]
+        Name:string;
+
+        [<field: DataMember(Name="Amount")>]
+        Amount:int; 
+    }
+
+[<DataContract>]
+type Order = 
+    {
+        [<field: DataMember(Name="Name")>]
+        Id:int; 
+
+        [<field: DataMember(Name="BuyerId")>]
+        BuyerId:int; 
+
+        [<field: DataMember(Name="Commodities")>]
+        Commodities:array<Commodity> 
+    }
+
+
+let private John:Customer = { Id=1; Name="John" }
+let private Ann:Customer = { Id=2; Name="Ann" }
+
+let private Apple:Commodity = { Id=1; Name="Apple"; Amount=100 }
+let private Banana:Commodity = { Id=2; Name="Banana"; Amount=200 }
+let private Shit:Commodity = { Id=3; Name="Shit"; Amount=100 }
+
+let private Order1:Order = { Id=1; BuyerId=1; Commodities = [| { Id=1; Name="Apple"; Amount=12 }  |] }
+
+
+type DB = 
+    static member Customers:List<Customer> = [ John; Ann; ]
+    static member Commodities = [ Apple; Banana; Shit; ]
+    static member Orders:List<Order> = [ Order1; ]        
--- Astral/Program.fs
+++ Astral/Program.fs
@@ -1,7 +1,57 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.Json
+
+open System.Text
+open System.Runtime.Serialization
+open System.Collections.Generic
+
+open Models
+
+let app3 =
+    choose
+        [ GET >=> choose
+            [ 
+                path "/customers" >=> mapJson (fun () -> List.toArray DB.Customers)
+                path "/commodities" >=> mapJson (fun () -> List.toArray DB.Commodities)
+                path "/orders" >=> mapJson (fun () -> List.toArray DB.Orders)
+                path "/hello" >=> OK "Hello!"
+
+                path "/" >=> OK "Main page"
+                path "/three" >=> Files.file "./client/three.html"
+                Files.browseHome
+
+                RequestErrors.NOT_FOUND "Page not found." 
+            ]
+        ]
+
+
+let app1 =
+    choose
+        [ GET >=> choose
+            [ 
+                path "/customers" >=> mapJson (fun () -> List.toArray DB.Customers)
+                path "/commodities" >=> mapJson (fun () -> List.toArray DB.Commodities)
+                path "/orders" >=> mapJson (fun () -> List.toArray DB.Orders)
+                path "/hello" >=> OK "Hello!"
+
+                path "/three" >=> OK "Lol" //Files.file "..\\..\\..\\client\\three.js"
+
+                RequestErrors.NOT_FOUND "Page not found." 
+            ]
+        ]
+
+
+let app2 = mapJson (fun a -> DB.Customers.Head)
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    printfn "Starting server at 8083..."
+    startWebServer defaultConfig app3
+    0
+
+
+
+
