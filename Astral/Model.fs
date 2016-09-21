9936e5106792ee62d061a0a8575d4e9693c34cca
zelinskiy <zelinskiy1917@gmail.com>
2016-09-21 20:55:53 +0300

    Immutable data structures

--- Astral/Model.fs
+++ Astral/Model.fs
@@ -15,78 +15,82 @@ type Point = {
 }
 
 [<DataContract(Name = "root")>]
-type AstralObject(name:string) = 
-    
+type AstralObject ={
     [<field: DataMember(Name="id")>]
-    member val Id = -1 with get, set
+    Id:int;
 
     [<field: DataMember(Name="name")>]
-    member val Name = name with get, set
+    Name:string;
 
     [<field: DataMember(Name="lecture")>]
-    member val Lecture = "Empty" with get, set
+    Lecture:string;
 
     [<field: DataMember(Name="position")>]
-    member val Position = { x=0.0; y=0.0; z=0.0; } with get, set
+    Position:Point;
 
     [<field: DataMember(Name="r")>]
-    member val Radius = 0.0 with get, set
+    Radius:double;
 
     [<field: DataMember(Name="rotV")>]
-    member val RotationVelocity = 0.0 with get, set
+    RotationVelocity:double;
 
     [<field: DataMember(Name="texture")>]
-    member val Texture = "" with get, set
+    Texture:string;
+} 
+    
+    
 
 [<DataContract(Name = "root")>]
-type AstralSystem(name:string) = 
-    
+type AstralSystem = {
     [<field: DataMember(Name="id")>]
-    member val Id = -1 with get, set
+    Id:int;
 
     [<field: DataMember(Name="name")>]
-    member val Name = name with get, set
+    Name:string;
 
     [<field: DataMember(Name="elements")>]
-    member val ElementsIds:array<int> = [| |] with get,set
+    ElementsIds:array<int>;
 
     [<field: DataMember(Name="picture")>]
-    member val Picture = "" with get, set
+    Picture:string;
+}
+    
+    
 
     
 
-let private Earth = new AstralObject("Earth")
-Earth.Id <- 1
-Earth.Lecture <- "Earth is our home" 
-Earth.Position <- { x=(-30.0); y=0.0; z=0.0; }
-Earth.Radius <- 10.0
-Earth.RotationVelocity <- 0.003
-Earth.Texture <- "/client/pictures/earth_texture.jpg"
+let private Earth = {
+    Id = 1;
+    Name="Earth";
+    Lecture = "Earth is our home";
+    Position = { x=(-30.0); y=0.0; z=0.0; };
+    Radius = 10.0;
+    RotationVelocity = 0.003;
+    Texture = "/client/pictures/earth_texture.jpg";
+}
 
-let private Pluto = new AstralObject("Pluto")
-Pluto.Id <- 2
-Pluto.Lecture <- "Pluto is merely a planet"
-Pluto.Position <- { x=35.0; y=0.0; z=30.0; }
-Pluto.Radius <- 3.0
-Pluto.RotationVelocity <- 0.015
-Pluto.Texture <- "/client/pictures/pluto_texture.jpg"
-
-let private Jupiter = new AstralObject("Jupiter")
-Jupiter.Id <- 3
-Jupiter.Lecture <- "Jupiter is the biggest Solar System planet"
-Jupiter.Position <- { x=0.0; y=0.0; z=(-70.0); }
-Jupiter.Radius <- 20.0
-Jupiter.RotationVelocity <- 0.015
-Jupiter.Texture <- "/client/pictures/jupiter_texture.jpg"
+let private Pluto = {
+    Id = 2;
+    Name="Pluto";    
+    Lecture = "Pluto is merely a planet";
+    Position = { x=35.0; y=0.0; z=30.0; };
+    Radius = 3.0;
+    RotationVelocity = 0.015;
+    Texture = "/client/pictures/pluto_texture.jpg";
+}
 
 
-let SolarSystem = new AstralSystem("Solar System")
-SolarSystem.ElementsIds <- [| 1; 2; 3 |]
-SolarSystem.Id <- 1
-SolarSystem.Picture <- "/client/pictures/solar_system.jpg"
+
+
+let SolarSystem = {
+    Name="Solar System";
+    ElementsIds = [| 1; 2 |];
+    Id = 1;
+    Picture = "/client/pictures/solar_system.jpg";
+}
 
 type DB = 
-    static member AstralObjects = [| Earth; Pluto; Jupiter |]
+    static member AstralObjects = [| Earth; Pluto |]
     static member AstralSystems = [| SolarSystem |]
 
     static member findObject (id:int) = 
